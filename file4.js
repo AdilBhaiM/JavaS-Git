663daa22f87ec698ede43e0b35806947f894b36a
AdilBhaiM <mrkabsiejossw@gmail.com>
2023-09-18 16:38:29 +0500

    file4 modi

--- file4.js
+++ file4.js
@@ -45,6 +45,7 @@
 
 
 
+
 // New keyword.
 
 // We can use new keyword to perform same task performing by prototype instead of "create.object()" method.
@@ -80,6 +81,7 @@
 
 
 
+
 // HasOwnProperty.
 
 
@@ -97,3 +99,148 @@
 
 
 // More About Prototype.
+
+// It was about arrays.
+
+
+
+
+
+
+// Classes
+
+// Classes use hoti hain function ki jagah or ye property return khud hi kr deeta ha or empty object bnana ki bhi koi zarorat ni hoti..
+
+
+// class createUser{
+//      constructor(firstName, lastName, email, age, address){
+//      this.firstName = firstName;
+//      this.lastName = lastName;
+//      this.email = email;
+//      this.age = age;
+//      this.address = address;
+//      }
+
+//      about(){
+//      return `${this.firstName} is ${this.age} years old.`;
+//      }
+//      is18(){
+//          return this.age >= 18;
+//      }
+//      sing(){
+//      return 'toon na na na la la ';
+//      }
+// }
+// const user1 = new createUser('harshit', 'vashsith', 'harshit@gmail.com', 9, "my address");
+// const user2 = new createUser('harsh', 'vashsith', 'harshit@gmail.com', 19, "my address");
+// const user3 = new createUser('mohit', 'vashsitha', 'harshit@gmail.com', 17, "my address");
+// console.log(user1);
+// console.log(user1.about());
+// console.log(user3.sing());
+
+
+
+
+// Extends property is used in classes that prevent us to not write content in classes except the main class.
+
+// class Animal {
+//     constructor(name, age){
+//         this.name = name;
+//         this.age = age;
+//     }
+
+//     eat(){
+//         return `${this.name} is eating`;
+//     }
+
+//     isSuperCute(){
+//         return this.age <= 1;
+//     }
+
+//     isCute(){
+//         return true;
+//     }
+// }
+
+// class Dog extends Animal{
+    
+// } 
+
+// const tommy = new Dog("tommy", 3);
+// console.log(tommy);
+// console.log(tommy.isCute());
+
+
+
+
+// Super Keyword
+
+// Super keyword is used in other classes to take values from main or extended class.
+
+
+// class Animal {
+//     constructor(name, age){
+//         this.name = name;
+//         this.age = age;
+//     }
+
+//     eat(){
+//         return `${this.name} is eating`;
+//     }
+
+//     isSuperCute(){
+//         return this.age <= 1;
+//     }
+
+//     isCute(){
+//         return true;
+//     }
+// }
+
+// class Dog extends Animal{
+//     constructor(name, age, speed){
+//         super(name,age);
+//         this.speed = speed;
+//     }
+
+//     run(){
+//         return `${this.name} is running at ${this.speed}kmph`
+//     }
+// } 
+// // object / instance 
+// const tommy = new Dog("tommy", 3,45);
+// console.log(tommy.run());
+
+
+
+
+
+// Getter and Setter
+
+// Is me do chezien han. Aik get ha jo func ke name se pehla likh deta han or is se hmara pas aik value call krna se dono values call ho ke method print ho jay ga.
+
+// Is me ham aik value ke set krna k bad uski value ko change kr k alehda se print krna sikhien ga..
+
+
+class Person{
+    constructor(firstName, lastName, age){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+    get fullName(){
+        return `${this.firstName} ${this.lastName}`
+    }
+    set fullName(fullName){
+        const [firstName, lastName] = fullName.split(" ");
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
+
+
+const person1 = new Person("harshit", "sharma", 5);
+console.log(person1.fullName);
+console.log(person1.fullName);
+person1.fullName = "mohit vashistha";
+console.log(person1);
